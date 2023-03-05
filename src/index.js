ae85bd0d72bfd068b968ad72dee1a937536f3fbf
Kishor5953 <114181652+Kishor5953@users.noreply.github.com>
2023-03-05 12:16:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Total from './Total';
-
+import App from './App';
 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <App />
     <Total />
     
 
